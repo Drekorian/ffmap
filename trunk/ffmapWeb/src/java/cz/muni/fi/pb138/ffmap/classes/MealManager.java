c2dc41a96e823822625e563e004ce9662873fdcd
325253@mail.muni.cz <325253@mail.muni.cz@094f7d37-6256-cd72-773b-3a310b43526c>
2011-06-24 20:21:48 +0000

    MealManager hotfix

--- trunk/ffmapWeb/src/java/cz/muni/fi/pb138/ffmap/classes/MealManager.java
+++ trunk/ffmapWeb/src/java/cz/muni/fi/pb138/ffmap/classes/MealManager.java
@@ -38,33 +38,20 @@ public class MealManager implements IDatabaseManager {
     }
 
     public IDatabaseStoreable find(long id) {
-        try {
-            String queryResult = database.XQueryCommand("for $meal in //meals return $meal");
-            System.out.println(queryResult);
-            Document doc = XQueryResultController.getDocument(queryResult);
-            Element node = doc.getElementById(String.valueOf(id));
-            if(node == null) System.out.println("Fail!");
-            long mealId = Long.valueOf(node.getAttribute("id"));
-            String name = node.getAttribute("name");
-            String description = node.getFirstChild().getNodeValue();
-            Meal meal = new Meal(name, null, description);
-            return meal;
-        } catch (Exception ex) {
-            Logger.getLogger(MealManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return null;
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     public List<? extends IDatabaseStoreable> getAll() {
-        List<Meal> result = new ArrayList<Meal>();
+        throw new UnsupportedOperationException();
+        /*List<Meal> result = new ArrayList<Meal>();
 
-        Document queryResult = XQueryResultController.getDocument(database.XQueryCommand("/fastfood-database/meals"));
-
+        Document queryResult = XQueryResultController.getDocument(database.XQueryCommand("/fastfood-database/meals"));*/
     }
     public long count() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    private parseMeal(Element meal, NodeList prices){
+    /*private parseMeal(Element meal, NodeList prices){
+        throw new UnsupportedOperationException();
+        
         Meal result = null;
 
         Long _id = Long.valueOf(meal.getAttribute("id"));
@@ -72,7 +59,6 @@ public class MealManager implements IDatabaseManager {
         if(meal.hasChildNodes()){
             String _description = meal.getElementsByTagName("description").item(0).getTextContent();
         }
-
-    }
+    }*/
 
 }
